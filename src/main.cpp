f5e84735949c9ff4ba2c4cdba617d3fd1d00f10d
Alex <alexciacciarella@gmail.com>
2025-02-25 22:11:21 +0100

    Fix

--- src/main.cpp
+++ src/main.cpp
@@ -21,9 +21,9 @@ int main() {
     }else if(input.find("echo ") == 0){
       std::string text = input.substr(5);
       std::cout << text << std::endl;
-    }else if (input.find("type echo")){
+    }else if (input.find("type echo") == 0){
       std::cout << input.substr(5) << " is a shell builtin" << std::endl;
-    }else if (input.find("type exit")){
+    }else if (input.find("type exit") == 0){
       std::cout << input.substr(5) << " is a shell builtin" << std::endl;
     }else{
       std::cout << input << ": command not found" << std::endl;
