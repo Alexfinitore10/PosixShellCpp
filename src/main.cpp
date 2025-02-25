daf7bbaa54aed57be723b4ec3ef282f28eaf42b9
Alex <alexciacciarella@gmail.com>
2025-02-25 21:46:51 +0100

    Third Stage

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,16 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  std::cout << "$ ";
+
+  //REPL
+  while (1)
+  {
+    std::cout << "$ ";
   
-  std::string input;
+    std::string input;
 
-  std::getline(std::cin, input);
+    std::getline(std::cin, input);
 
-  std::cout << input << ": command not found" << std::endl;
+    std::cout << input << ": command not found" << std::endl;
+  }
 }
