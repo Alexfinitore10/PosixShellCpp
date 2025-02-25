6d5d8f11c6596213d485510830263a4aee8c10a7
Alex <alexciacciarella@gmail.com>
2025-02-25 22:27:42 +0100

    Fix

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,12 @@ int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-
+  std::string input;
 
   //REPL
   while (1)
   {
     std::cout << "$ ";
-  
-    std::string input;
 
     std::getline(std::cin, input);
 
@@ -21,17 +19,12 @@ int main() {
     }else if(input.find("echo ") == 0){
       std::string text = input.substr(5);
       std::cout << text << std::endl;
-    }else if (input.find("type " == 0))
-    {
+    }else if (input.substr(0,5) == "type "){
       std::string tipo = input.substr(5);
-      if(tipo == "echo"){
-        std::cout << input.substr(5) << " is a shell builtin" << std::endl;
-      }else if (tipo == "exit"){
-        std::cout << input.substr(5) << " is a shell builtin" << std::endl;
-      }else if (tipo == "type"){
-        std::cout << input.substr(5) << " is a shell builtin" << std::endl;
+      if(tipo.substr(0,4) == "echo" || tipo.substr(0,4) == "type" || tipo.substr(0,4) == "exit"){
+        std::cout << tipo << " is a shell builtin" << std::endl;
       }else{
-        std::cout << input.substr(5) << ": not found" << std::endl;
+        std::cout << tipo << ": not found" << std::endl;
       }
     }else{
       std::cout << input << ": command not found" << std::endl;
