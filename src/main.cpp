4f4b9f479da8c6732dbe415c7372053a4dc1a26e
Alex <alexciacciarella@gmail.com>
2025-03-04 22:23:47 +0100

    Type Task

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <sstream>
+
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -21,10 +23,23 @@ int main() {
       std::cout << text << std::endl;
     }else if (input.substr(0,5) == "type "){
       std::string tipo = input.substr(5);
+      //Discriminating if it's NOT an executable file (env)
       if(tipo.substr(0,4) == "echo" || tipo.substr(0,4) == "type" || tipo.substr(0,4) == "exit"){
         std::cout << tipo << " is a shell builtin" << std::endl;
       }else{
-        std::cout << tipo << ": not found" << std::endl;
+        bool found = false;
+        const char * path = std::getenv("PATH");//mi prendo il path
+        std::istringstream path_stream(path);
+        std::string buffer;
+        while(std::getline(path_stream, buffer, ':')){
+          if(buffer.find(tipo))
+          {
+            std::cout<<tipo<<" is "<<buffer<<std::endl;
+            found = true;
+            break;
+          }
+        }
+        if(found == false)std::cout << tipo << ": not found" << std::endl;
       }
     }else{
       std::cout << input << ": command not found" << std::endl;
