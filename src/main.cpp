082f44883fdaab35f1e4d4f6de47f0d5e336a672
Alex <alexciacciarella@gmail.com>
2025-03-09 10:02:57 +0100

    Fixing but not finished yet, going to rebuild some of the code

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,25 @@
 #include <sstream>
 #include <cstdlib>
 #include <filesystem>
+#include <vector>
+
 
 #include <chrono>
 
-std::filesystem::path isInPath(std::string path, std::string input)
-{
-  std::istringstream path_stream(path);
-  std::string buffer;
-        
-  while(std::getline(path_stream, buffer, ':')){
-    std::filesystem::path patto = std::filesystem::path(buffer) / input;
-    if(std::filesystem::exists(patto))//mannaggia guarda
-    {
-      return patto;
-    }
-  }
-  return "";
-}
+
+
+enum Type{
+  Executable,
+  Builtin,
+  NotFound,
+};
+
+
+std::vector<std::string> parsingTheCommand(std::string);
+std::filesystem::path isInPath(std::string, std::string);
+std::pair<Type, std::string> generateFullCommand(std::string);
+
+ 
 
 
 int main(int argc, char* argv[]) {
@@ -36,6 +38,13 @@ int main(int argc, char* argv[]) {
 
     std::getline(std::cin, input);
 
+    std::vector<std::string> parsedCommand = parsingTheCommand(input);
+    
+    /* for (auto a : parsedCommand)
+    {
+      std::cout<<a<<std::endl;
+    } */
+    
     if(input == "exit 0")
     {
       //auto const t1 = std::chrono::high_resolution_clock::now();
@@ -77,6 +86,54 @@ int main(int argc, char* argv[]) {
         system(patto.c_str());
       }else std::cout << input << ": command not found" << std::endl;
     }
+  }
+}
+
+std::vector<std::string> parsingTheCommand(std::string input)
+{
+  std::vector<std::string> vec;
+  std::string word_acc;
+
+  for (char c : input)
+  {
+    if (c == ' ')
+    {
+      vec.push_back(word_acc);
+      word_acc = "";
+    }else{
+      word_acc += c;
+    }
+  }
+  if (word_acc != "")
+  {
+    vec.push_back(word_acc);
+  }
+
+  return vec;
+}
+
+std::filesystem::path isInPath(std::string path, std::string input)
+{
+  std::istringstream path_stream(path);
+  std::string buffer;
+        
+  while(std::getline(path_stream, buffer, ':')){
+    std::filesystem::path patto = std::filesystem::path(buffer) / input;
+    if(std::filesystem::exists(patto))//mannaggia guarda
+    {
+      return patto;
+    }
+  }
+  return "";
+}
+
+std::pair<Type, std::string> generateFullCommand(std::string comando)
+{
+  std::pair<Type, std::string> comandoCoppia;
+  //comadno puo essere BUIltin, executable, not found
+  if(comando == "echo" || comando == "exit" || comando == "type")
+  {
+    //TODO
   }
 }
 
